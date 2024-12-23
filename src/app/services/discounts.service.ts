719348d7bdee76fb3a5798f14b1a98e590e85390
lennartmac <lennartpopma@hotmail.com>
2024-12-23 11:28:38 +0100

    Use manifest.json to retrieve discounts.version.json (no full app redeploy needed for data changes)

--- src/app/services/discounts.service.ts
+++ src/app/services/discounts.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DiscountsService {
-  private url = 'assets/discounts.v2.json';
+  private manifestUrl = 'assets/manifest.json';
 
   constructor(private http: HttpClient) {}
 
   getDiscounts(): Observable<string[]> {
-    return this.http.get<string[]>(this.url);
+    const timestamp = new Date().getTime();
+    const cacheBustedUrl = `${this.manifestUrl}?cache-bust=${timestamp}`;
+
+    return this.http.get<{ latestDiscounts: string }>(cacheBustedUrl).pipe(
+      switchMap(manifest => this.http.get<string[]>(`assets/${manifest.latestDiscounts}`))
+    );
   }
 }
